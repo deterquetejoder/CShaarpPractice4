a3fdfb65e9756cff18ad6704cfb771d4b10bb2a3
deterquetejoder <saecularisator@gmail.com>
2023-08-06 16:08:07 +0500

    ts29

---
+++ ts29/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int Input(string msg)
+{
+    Console.WriteLine("Enter your number to get the sum of digits in it");
+ int num = Convert.ToInt32(Console.ReadLine());
+}
+
