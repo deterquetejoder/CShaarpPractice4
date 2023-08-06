1c7b50e54901a74f8e37ac50c00049f6d8c697c8
deterquetejoder <saecularisator@gmail.com>
2023-08-06 15:54:27 +0500

    ts27 works with strings

--- ts27/Program.cs
+++ ts27/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+ Console.WriteLine("Enter your number to get the sum of digits in it");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+if(num<0)num*=-1;
+string number = num.ToString();
+int sum = 0;
+for (int i = 0; i < number.Length; i++) {
+    sum += number[i] - '0';
+}
+Console.WriteLine($"The sum is {sum}");
