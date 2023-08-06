1c6517f6d9b7b6e3b3716042f2e25d5829872575
deterquetejoder <saecularisator@gmail.com>
2023-08-06 16:15:58 +0500

    ts29 works

--- ts29/Program.cs
+++ ts29/Program.cs
@@ -1,7 +1,36 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 int Input(string msg)
 {
-    Console.WriteLine("Enter your number to get the sum of digits in it");
- int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(msg);
+ int res = Convert.ToInt32(Console.ReadLine());
+ return res;
 }
 
+int size =8;
+
+int[] FillArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    Random rand = new Random();
+    for(int i = 0;i<size;i++)
+    {
+        array[i]=rand.Next(minValue,maxValue+1);
+    } return array;
+
+
+
+
+}
+void PrintArray(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+int minValue = Input("Enter the minimal value of the array's elements");
+int maxValue = Input("Enter the maximal value of the array's elements");
+
+PrintArray(FillArray(size,minValue,maxValue));
