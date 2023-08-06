fe19b499b9f31e1b3e33374144c53913a8ad0ffc
deterquetejoder <saecularisator@gmail.com>
2023-08-06 15:20:09 +0500

    ts25 done

---
+++ ts25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("enter A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+
+
+if (b<1)
+Console.Write("number b must be natural");
+else
+{
+for(;b!=0;b=b-1)
+{
+c = c * a;
+}
+Console.Write($"Number A  B times equals {c}");
+}
